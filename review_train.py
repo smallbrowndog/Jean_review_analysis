15a69047ef107062ac5cbf6a3db0674895c21bc5
smallbrowndog <kkj7388@naver.com>
2024-12-10 23:10:05 +0900

    update

--- review_train.py
+++ review_train.py
@@ -1,37 +1,37 @@
 # 트레인데이터 수집
 
 import pandas as pd
+import random
 
 data = pd.read_csv('./Data/ALL_data.csv')
 # print(data)
 
-not_pos_data = data[(data['별점']<=3) & (data['별점']>0)]
-random_pos_data = not_pos_data.sample(n=1147)
+select_ratio = 0.7
 
-# 조건에 맞는 인덱스 중 랜덤하게 30% 추출
-random_not_pos_data = list(neg)
+# 3점 이하 모으기
+neg_data = data[data['별점']<=3]
+
+# 랜덤하게 70% 추출
 random.seed(42)
-random.shuffle(neg)
-# print(neg)
+neg_data_rd = neg_data.sample(frac=select_ratio)
 
-select_ratio = 0.3
-
-neg = neg[:int(select_ratio * len(neg))]
-selected_neg = project.iloc[neg]
-print(selected_neg)
-
-print(f'긍정이 아닌 데이터: \n{not_pos_data}')
-
-pos_data = data[data['별점']==5]
-# print(pos_data)
-random_pos_data = pos_data.sample(n=1147)
-print(f'긍정 데이터: \n{random_pos_data}')
+print(f'총 부정 데이터: {len(neg_data)}\n'
+      f'30% 추출 부정 데이터: {len(neg_data_rd)}\n{neg_data_rd}')
 
 
+# 4점 이상 모으기
+pos_data = data[data['별점']>=4]
+
+# 랜덤하게 70% 추출
+random.seed(42)
+pos_data_rd = pos_data.sample(frac=select_ratio)
+
+print(f'총 긍정 데이터: {len(pos_data)}\n'
+      f'30% 추출 긍정 데이터: {len(pos_data_rd)}\n{pos_data_rd}')
 
 train_data = []
 
-train_data = pd.concat([not_pos_data, random_pos_data], axis=0)
+train_data = pd.concat([neg_data_rd, pos_data_rd], axis=0)
 print(f'학습 데이터: \n{train_data}')
 
 train_data.to_csv('./Data/train_data.csv', index=False, encoding='utf-8-sig')
